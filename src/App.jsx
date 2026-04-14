4404d1f1f0aee8f8e009c8926c69d276bf999291
Fredde <fredderppred@gmail.com>
2026-04-14 09:25:34 +0200

    Added analytics tracking to page visits

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,38 @@
-import { Route, Routes } from "react-router";
+import { Route, Routes, useLocation } from "react-router-dom";
+import { useEffect } from "react";
+
 import HomePage from "./pages/HomePage.jsx";
 import SpecializationPage from "./pages/SpecializationPage.jsx";
 import SpitePage from "./pages/SpitePage.jsx";
 import AliensPage from "./pages/AliensPage.jsx";
 import CV from "./pages/CV.jsx";
+import { initGA, trackPage } from "./analytics.jsx";
+
+function AnalyticsTracker() {
+  const location = useLocation();
+
+  useEffect(() => {
+    trackPage(location.pathname);
+  }, [location.pathname]);
+
+  return null;
+}
 
 export default function App() {
+  useEffect(() => {
+    initGA();
+  }, []);
+
   return (
-    <Routes>
-      <Route path="/" element={<HomePage />} />
-      <Route path="/project/specialization" element={<SpecializationPage />} />
-      <Route path="/project/aliens-stole-my-ship" element={<AliensPage />} />
-      <Route path="/project/spite-blood-and-gold" element={<SpitePage />} />
-      <Route path="/cv" element={<CV />} />
-    </Routes>
+    <>
+      <AnalyticsTracker />
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/project/specialization" element={<SpecializationPage />} />
+        <Route path="/project/aliens-stole-my-ship" element={<AliensPage />} />
+        <Route path="/project/spite-blood-and-gold" element={<SpitePage />} />
+        <Route path="/cv" element={<CV />} />
+      </Routes>
+    </>
   );
 }
